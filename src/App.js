9aefcbe5aed4e9d11e67ce36aefc0a40d575f4f1
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-10-23 00:17:55 +0200

    adding images to assets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 /** @format */
 
-import logo from './logo.svg';
+import assets from './assets/index';
 import './App.less';
 
 function App() {
-	return <div className="App">Hello Word</div>;
+	return (
+		<div className="App">
+			Hello Word
+			<img src={assets.images.mod_CTA_img1} className="App-logo" alt="logo" />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/assets/index.js
@@ -0,0 +1,42 @@
+/** @format */
+import logo from './images/logo.svg';
+import header_basic_img1 from './images/header-basic_img1.svg';
+import header_basic_img2 from './images/header-basic_img2.svg';
+import mod_small_img1 from './images/mod-small_img1.svg';
+import mod_small_img2 from './images/mod-small_img2.svg';
+import mod_small_img3 from './images/mod-small_img3.svg';
+import mod_small_img4 from './images/mod-small_img4.svg';
+import mod_icons_icon1 from './images/mod-icons_icon1.svg';
+import mod_icons_icon2 from './images/mod-icons_icon2.svg';
+import mod_icons_icon3 from './images/mod-icons_icon3.svg';
+import mod_icons_icon4 from './images/mod-icons_icon4.svg';
+import mod_icons_icon5 from './images/mod-icons_icon5.svg';
+import mod_points_img1 from './images/mod-points_img1.svg';
+import mod_points_list_icon from './images/mod-points_list-icon.svg';
+import mod_logos_img1 from './images/mod-logos_img1.svg';
+import mod_logos_img2 from './images/mod-logos_img2.svg';
+import mod_CTA_img1 from './images/mod-CTA_img1.svg';
+
+const assets = {
+	images: {
+		logo: logo,
+		header_basic_img1: header_basic_img1,
+		header_basic_img2: header_basic_img2,
+		mod_small_img1: mod_small_img1,
+		mod_small_img2: mod_small_img2,
+		mod_small_img3: mod_small_img3,
+		mod_small_img4: mod_small_img4,
+		mod_icons_icon1: mod_icons_icon1,
+		mod_icons_icon2: mod_icons_icon2,
+		mod_icons_icon3: mod_icons_icon3,
+		mod_icons_icon4: mod_icons_icon4,
+		mod_icons_icon5: mod_icons_icon5,
+		mod_points_img1: mod_points_img1,
+		mod_points_list_icon: mod_points_list_icon,
+		mod_logos_img1: mod_logos_img1,
+		mod_logos_img2: mod_logos_img2,
+		mod_CTA_img1: mod_CTA_img1,
+	},
+};
+
+export default assets;
