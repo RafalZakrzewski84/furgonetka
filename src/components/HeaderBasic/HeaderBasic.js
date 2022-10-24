ec53cf9c2dc19ae5819cc8d65fcafe40957b8d3b
Rafal Zakrzewski <rzakrzewski84@gmail.com>
2022-10-24 14:51:54 +0200

    styling of HeaderBasic

--- src/components/HeaderBasic/HeaderBasic.js
+++ src/components/HeaderBasic/HeaderBasic.js
@@ -9,16 +9,33 @@ function HeaderBasic({ images, texts }) {
 			<div className="HeaderBasic__container">
 				<div className="HeaderBasic__content">
 					<div className="HeaderBasic__box">
-						<p>{texts.topCaption.toUpperCase()}</p>
-						<h1>
-							{texts.h1Text}
-							<span>{texts.h1Span}</span>
-						</h1>
-						<img src={images.marker} alt="marker" />
-						<p>{texts.headerText}</p>
+						<div className="HeaderBasic__box-container">
+							<div className="HeaderBasic__box-content">
+								<div className="HeaderBasic__header">
+									<p className="HeaderBasic__topCaption">
+										{texts.topCaption.toUpperCase()}
+									</p>
+									<h1 className="HeaderBasic__h1Text">
+										{texts.h1Text}
+										<span className="HeaderBasic__h1Span">{texts.h1Span}</span>
+									</h1>
+									<img
+										className="HeaderBasic__marker"
+										src={images.marker}
+										alt="marker"
+									/>
+								</div>
+								<p className="HeaderBasic__headerText">{texts.headerText}</p>
+							</div>
+							<button>Button</button>
+						</div>
 					</div>
 					<div className="HeaderBasic__box">
-						<img src={images.image} alt="header basic" />
+						<img
+							className="HeaderBasic__img"
+							src={images.image}
+							alt="header basic"
+						/>
 					</div>
 				</div>
 			</div>
